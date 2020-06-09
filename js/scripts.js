bed28c5f85442042835ff8ab326d1c484be3d194
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-09 11:57:36 -0700

    add logic for incorrect form entries
    co-authored-by: Teresa Rosinski <trosinski89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,27 @@
 
 $(document).ready(function() {
   $("#form").submit(function(event) {
+  
     const countTo = parseInt($("input#countTo").val());
     const countBy = parseInt($("input#countBy").val());
     const startAt = parseInt($("input#startAt").val());
     const numbersArray = [];
-    for (let i = startAt; i <= countTo; i += countBy){
-      numbersArray.push(" " + i);
-    }
+
+    if (countBy > countTo) {
+      alert("Please enter a smaller number to count by");
+    } else if (countTo >= 0 && countBy >= 0 && startAt >=0) {
+      for (let i = startAt; i <= countTo; i += countBy){
+        numbersArray.push(" " + i);
+      }
+
+  // UI logic
+
+      $("#result").show();
+      $("#output").text("Your calculation is " + numbersArray + " and the total amount of numbers in your calcuation is " + (numbersArray.length));
+  } else {
+    alert("Please enter positive integers in all fields");
+  }
     event.preventDefault();
-
-// UI logic
-
-$("#result").show();
-$("#output").text("Your calculation is " + numbersArray + " and the total amount of numbers in your calcuation is " + (numbersArray.length));
 
 });
 });
