2448bfb0e015bd82d286fd7140e49fadb02309fb
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-09 11:35:38 -0700

    add UI logic to show result, made loop add spaces to array
    co-authored-by: Teresa Rosinski <trosinski89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// business logic
+
 $(document).ready(function() {
   $("#form").submit(function(event) {
     const countTo = parseInt($("input#countTo").val());
@@ -5,9 +7,14 @@ $(document).ready(function() {
     const startAt = parseInt($("input#startAt").val());
     const numbersArray = [];
     for (let i = startAt; i <= countTo; i += countBy){
-      numbersArray.push(i);
-  }
-      console.log(numbersArray);
-  event.preventDefault();
-  });
+      numbersArray.push(" " + i);
+    }
+    event.preventDefault();
+
+// UI logic
+
+$("#result").show();
+$("#output").text("Your calculation is " + numbersArray + " and the total amount of numbers in your calcuation is " + (numbersArray.length));
+
+});
 });
